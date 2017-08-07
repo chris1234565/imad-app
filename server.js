b5168975d61ea21c4828370e6459583e24a2f287
chris1234565 <30677363+chris1234565@users.noreply.github.com>
2017-08-07 16:15:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,7 +77,11 @@ app.get('/:articleName',function(req,res){
 var articleName=req.params.articleName;
 res.send(createtemplate(articles[articleName]));
 });
-
+var counter=0;
+app.get('/counter',function(req,res){
+   counter=counter+1;
+   res.send(counter.toString());
+});
 
 
 app.get('/ui/style.css', function (req, res) {
