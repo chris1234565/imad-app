5ce7e143797419ef7157c7aac98a907312f28922
chris1234565 <30677363+chris1234565@users.noreply.github.com>
2017-08-07 15:40:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,8 @@ console.log('Loaded!');
 var element=document.getElementById('main');
 
 element.innerHTML='i too love you';
-\\move image
+
 var image=document.getElementById('madi');
-image.onclick=function{
+image.onclick=function(){
     image.style.marginLeft='100px';
 };
