c974b9f88e70c900fde66a491cf126327f899028
chris1234565 <30677363+chris1234565@users.noreply.github.com>
2017-08-07 17:07:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,10 +12,9 @@ image.onclick=function(){
 };
 
 var counter=0;
-
 var button=document.getElementById('counter');
 button.onclick=function(){
   counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHTML=counter.toString();
+  var sp=document.getElementById('count');
+  sp.innerHTML=counter.toString();
 };
