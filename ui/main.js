6236f276e46824346f2e7dda391c49f1ec4a2785
chris1234565 <30677363+chris1234565@users.noreply.github.com>
2017-08-07 16:48:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,20 @@
 console.log('Loaded!');
 var element=document.getElementById('main');
-
 element.innerHTML='Cool';
-
 var image=document.getElementById('madi');
 var marginLeft=0;
-
 function moveRight(){
     marginLeft=marginLeft+10;
     image.style.marginLeft=marginLeft+'px';
 }
 image.onclick=function(){
    var interval= setInterval(moveRight,100);
+};
+
+var c=0;
+var button=document.getElementById('counter');
+button.onclick=function(){
+  c=c+1;
+  var span=document.getElementById('count');
+  span.innerHTML=c.toString();
 };
