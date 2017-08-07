1236089c5e0e05f9ff1f58cee5ce25c9cb5287a1
chris1234565 <30677363+chris1234565@users.noreply.github.com>
2017-08-07 15:46:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element=document.getElementById('main');
 element.innerHTML='i too love you';
 
 var image=document.getElementById('madi');
+var marginLeft=0;
+
+function moveRight(){
+    marginLeft=marginLeft+10;
+    image.style.marginLeft=marginLeft+'px';
+}
 image.onclick=function(){
-    image.style.marginLeft='100px';
+   var interval= setInterval(moveRight,100);
 };
