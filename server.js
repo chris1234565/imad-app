1a098a50af909a17b4f4f871c72a7cebb0bb1918
chris1234565 <30677363+chris1234565@users.noreply.github.com>
2017-08-21 21:39:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('articles/:articleName',function(req,res){
+app.get('articles/article-one',function(req,res){
 pool.query("SELECT * FROM test where name=hello",function(err,result){
     if(err){
         res.status(500).send(error.toString());
